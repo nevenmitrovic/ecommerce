62638d47073989222e5f718ed765257cec6112d5
nevenmitrovic <nevenmitrovic4@gmail.com>
2025-05-24 14:56:38 +0200

    carousel mobile ui

---
+++ src/components/product-page/carousel/Carousel.tsx
@@ -0,0 +1,38 @@
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import "./carousel.style.css";
+import Slider from "react-slick";
+
+interface CarouselProps {
+  images: string[];
+}
+
+const Carousel = ({ images }: CarouselProps) => {
+  const settings = {
+    dots: true,
+    fade: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    waitForAnimate: false,
+  };
+
+  return (
+    <Slider {...settings}>
+      {images.map((img, index) => {
+        return (
+          <div key={img} className="carousel-img-container">
+            <img
+              className="carousel-img"
+              src={img}
+              alt={`product-image-${index}`}
+            />
+          </div>
+        );
+      })}
+    </Slider>
+  );
+};
+
+export default Carousel;
