acf2af84ad1956bb8b2aaec154119f3886ef8932
nevenmitrovic <nevenmitrovic4@gmail.com>
2025-05-24 10:44:44 +0200

    catalog page desktop UI

--- src/components/catalog-page/products/product-card/ProductCard.tsx
+++ src/components/catalog-page/products/product-card/ProductCard.tsx
@@ -17,8 +17,8 @@ const ProductCard = ({
     <div className="product-card">
       <img src={img} alt={title} />
       <div className="product-title">{title}</div>
-      <div className="product-regular-price">${regular_price}</div>
-      <div className="product-price">${sale_price}</div>
+      <div className="product-regular-price">${regular_price.toFixed(2)}</div>
+      <div className="product-price">${sale_price.toFixed(2)}</div>
     </div>
   );
 };
