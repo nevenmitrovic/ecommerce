daaaac72bb6a53ba3d448072ebe0db623f170b72
nevenmitrovic <nevenmitrovic4@gmail.com>
2025-06-10 12:09:40 +0200

    main layout height fixed

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,15 +1,15 @@
-import Navbar from "@/components/common/navbar/Navbar";
-import Footer from "@/components/common/footer/Footer";
-import { ToastContainer } from "react-toastify";
+import Navbar from '@/components/common/navbar/Navbar';
+import Footer from '@/components/common/footer/Footer';
+import { ToastContainer } from 'react-toastify';
 
 const MainLayout = ({ children }: any) => {
-  return (
-    <>
-      <Navbar />
-      <main>{children}</main>
-      <Footer />
-      <ToastContainer />
-    </>
-  );
+	return (
+		<div className='main-layout'>
+			<Navbar />
+			<main>{children}</main>
+			<Footer />
+			<ToastContainer />
+		</div>
+	);
 };
 export default MainLayout;
