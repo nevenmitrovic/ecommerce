6b57dc5bfb6bebf591f3342b30fe078a2b0a9375
nevenmitrovic <nevenmitrovic4@gmail.com>
2025-06-17 13:56:20 +0200

    vercel.json

--- src/App.tsx
+++ src/App.tsx
@@ -30,7 +30,7 @@ function App() {
 					<Route path='/cart' element={<Cart />} />
 					<Route path='/profile' element={<Profile />} />
 					<Route path='/about' element={<About />} />
-					<Route path='*' element={<NotFound />} />
+					<Route path='/*' element={<NotFound />} />
 				</Routes>
 			</CartContextProvider>
 		</UserContextProvider>
